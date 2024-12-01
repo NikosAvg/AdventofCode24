625ee3488e6d4562fb3520926a8bc7919282c7e9
Nikos <nik.avg@hotmail.com>
2024-12-01 11:06:46 +0200

    Day 1 Solution Part 2 Added

--- Day01/solution.py
+++ Day01/solution.py
@@ -8,8 +8,20 @@ l2 = df[1].sort_values()
 l1 = list(l1)
 l2 = list(l2)
 
+# Part one
 res = 0
 for i1, i2 in zip(l1,l2):
     res += abs(i2-i1)
     
 print(res)
+
+res2 = 0
+# Part two
+for i1 in l1:
+    count=0
+    for i2 in l2:
+        if i1 == i2:
+            count+=1
+    res2+=i1*count
+print(res2)
+
