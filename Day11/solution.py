cdf39146d44e8e71dca0d55c52caf4df09b0c18e
Nikos <nik.avg@hotmail.com>
2024-12-11 09:01:44 +0200

    Added Solution for Day 11

--- Day11/solution.py
+++ Day11/solution.py
@@ -1,37 +1,40 @@
-from collections import deque
+from collections import defaultdict, deque
 
 data = open('input.txt').read().strip().split(' ')
-def parse_stone(stone,new_stones):
+
+def parse_stone(stone,dp):
+    if stone in dp:
+        return dp[stone]
+
     if int(stone) == 0:
-        new_stones.append(str(1))
+        dp[stone] = ['1']
     elif len(stone) % 2 == 0:
         l_stone = int(stone[:len(stone)//2])
         r_stone = int(stone[len(stone)//2:])
-        new_stones.append(str(l_stone))
-        new_stones.append(str(r_stone))
+        dp[stone] = [str(l_stone), str(r_stone)]
     else:
-        stone = str(int(stone) * 2024)
-        new_stones.append(stone)
+        dp[stone] = [str(int(stone) * 2024)]
 
+    return dp[stone]
 
+dp = {}
 blinks = 25
-for blink in range(blinks):
+for _ in range(blinks):
     new_stones = deque()
     for d in data:
-        parse_stone(d,new_stones)
+        new_stones.extend(parse_stone(d,dp))
     data = list(new_stones)
 res = len(data)
 
 print(f'Part One {res}')
-
+dp = {}
 data = open('input.txt').read().strip().split(' ')
-res2 = 0
 blinks = 75
-for blink in range(blinks):
+for _ in range(blinks):
     new_stones = deque()
     for d in data:
-        parse_stone(d,new_stones)
+        new_stones.extend(parse_stone(d,dp))
     data = list(new_stones)
-res = len(data)
+res2 = len(data)
 
 print(f'Part Two {res2}')
